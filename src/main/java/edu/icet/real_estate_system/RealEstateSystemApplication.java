be81b5716017309bf21830baaec489fe6aba8593
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-11 16:28:19 +0530

    initialized spring boot project and configured mysql

---
+++ src/main/java/edu/icet/real_estate_system/RealEstateSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.icet.real_estate_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/icet/real_estate_system/RealEstateSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icet.real_estate_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
